6eaab23783b58c21630fe6b844596e191a63caac
Socheat KHAUV <pkayjava@gmail.com>
2017-10-25 23:07:34 +0800

    share charge clean

--- src/main/java/com/angkorteam/fintech/pages/charge/ShareChargeCreatePage.java
+++ src/main/java/com/angkorteam/fintech/pages/charge/ShareChargeCreatePage.java
@@ -3,6 +3,7 @@ package com.angkorteam.fintech.pages.charge;
 import java.util.List;
 
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
@@ -24,6 +25,8 @@ import com.angkorteam.fintech.provider.ChargeTimeProvider;
 import com.angkorteam.fintech.provider.CurrencyProvider;
 import com.angkorteam.fintech.provider.SingleChoiceProvider;
 import com.angkorteam.fintech.widget.TextFeedbackPanel;
+import com.angkorteam.fintech.widget.WebMarkupBlock;
+import com.angkorteam.fintech.widget.WebMarkupBlock.Size;
 import com.angkorteam.framework.models.PageBreadcrumb;
 import com.angkorteam.framework.wicket.ajax.form.OnChangeAjaxBehavior;
 import com.angkorteam.framework.wicket.markup.html.form.Button;
@@ -37,43 +40,57 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 @AuthorizeInstantiation(Function.ALL_FUNCTION)
 public class ShareChargeCreatePage extends Page {
 
-    private Form<Void> form;
-    private Button saveButton;
-    private BookmarkablePageLink<Void> closeLink;
+    protected Form<Void> form;
+    protected Button saveButton;
+    protected BookmarkablePageLink<Void> closeLink;
 
-    private String nameValue;
-    private TextField<String> nameField;
-    private TextFeedbackPanel nameFeedback;
+    protected WebMarkupBlock nameBlock;
+    protected WebMarkupContainer nameIContainer;
+    protected String nameValue;
+    protected TextField<String> nameField;
+    protected TextFeedbackPanel nameFeedback;
 
-    private CurrencyProvider currencyProvider;
-    private Option currencyValue;
-    private Select2SingleChoice<Option> currencyField;
-    private TextFeedbackPanel currencyFeedback;
+    protected WebMarkupBlock currencyBlock;
+    protected WebMarkupContainer currencyIContainer;
+    protected CurrencyProvider currencyProvider;
+    protected Option currencyValue;
+    protected Select2SingleChoice<Option> currencyField;
+    protected TextFeedbackPanel currencyFeedback;
 
-    private ChargeTimeProvider chargeTimeProvider;
-    private Option chargeTimeValue;
-    private Select2SingleChoice<Option> chargeTimeField;
-    private TextFeedbackPanel chargeTimeFeedback;
+    protected WebMarkupBlock chargeTimeBlock;
+    protected WebMarkupContainer chargeTimeIContainer;
+    protected ChargeTimeProvider chargeTimeProvider;
+    protected Option chargeTimeValue;
+    protected Select2SingleChoice<Option> chargeTimeField;
+    protected TextFeedbackPanel chargeTimeFeedback;
 
-    private ChargeCalculationProvider chargeCalculationProvider;
-    private Option chargeCalculationValue;
-    private Select2SingleChoice<Option> chargeCalculationField;
-    private TextFeedbackPanel chargeCalculationFeedback;
+    protected WebMarkupBlock chargeCalculationBlock;
+    protected WebMarkupContainer chargeCalculationIContainer;
+    protected ChargeCalculationProvider chargeCalculationProvider;
+    protected Option chargeCalculationValue;
+    protected Select2SingleChoice<Option> chargeCalculationField;
+    protected TextFeedbackPanel chargeCalculationFeedback;
 
-    private Double amountValue;
-    private TextField<Double> amountField;
-    private TextFeedbackPanel amountFeedback;
+    protected WebMarkupBlock amountBlock;
+    protected WebMarkupContainer amountIContainer;
+    protected Double amountValue;
+    protected TextField<Double> amountField;
+    protected TextFeedbackPanel amountFeedback;
 
-    private boolean activeValue;
-    private CheckBox activeField;
-    private TextFeedbackPanel activeFeedback;
+    protected WebMarkupBlock activeBlock;
+    protected WebMarkupContainer activeIContainer;
+    protected Boolean activeValue;
+    protected CheckBox activeField;
+    protected TextFeedbackPanel activeFeedback;
 
-    private SingleChoiceProvider taxGroupProvider;
-    private Option taxGroupValue;
-    private Select2SingleChoice<Option> taxGroupField;
-    private TextFeedbackPanel taxGroupFeedback;
+    protected WebMarkupBlock taxGroupBlock;
+    protected WebMarkupContainer taxGroupIContainer;
+    protected SingleChoiceProvider taxGroupProvider;
+    protected Option taxGroupValue;
+    protected Select2SingleChoice<Option> taxGroupField;
+    protected TextFeedbackPanel taxGroupFeedback;
 
-    private static final List<PageBreadcrumb> BREADCRUMB;
+    protected static final List<PageBreadcrumb> BREADCRUMB;
 
     @Override
     public IModel<List<PageBreadcrumb>> buildPageBreadcrumb() {
@@ -108,9 +125,11 @@ public class ShareChargeCreatePage extends Page {
     }
 
     @Override
-    protected void onInitialize() {
-        super.onInitialize();
+    protected void initData() {
+    }
 
+    @Override
+    protected void initComponent() {
         this.form = new Form<>("form");
         this.add(this.form);
 
@@ -121,61 +140,124 @@ public class ShareChargeCreatePage extends Page {
         this.closeLink = new BookmarkablePageLink<>("closeLink", ChargeBrowsePage.class);
         this.form.add(this.closeLink);
 
-        this.nameField = new TextField<>("nameField", new PropertyModel<>(this, "nameValue"));
-        this.nameField.setLabel(Model.of("Name"));
-        this.nameField.setRequired(true);
-        this.form.add(this.nameField);
-        this.nameFeedback = new TextFeedbackPanel("nameFeedback", this.nameField);
-        this.form.add(this.nameFeedback);
+        initNameBlock();
 
-        this.currencyProvider = new CurrencyProvider();
-        this.currencyField = new Select2SingleChoice<>("currencyField", 0, new PropertyModel<>(this, "currencyValue"), this.currencyProvider);
-        this.currencyField.setLabel(Model.of("Currency"));
-        this.currencyField.setRequired(true);
-        this.form.add(this.currencyField);
-        this.currencyFeedback = new TextFeedbackPanel("currencyFeedback", this.currencyField);
-        this.form.add(this.currencyFeedback);
+        initCurrencyBlock();
 
-        this.chargeTimeProvider = new ChargeTimeProvider();
-        this.chargeTimeProvider.setValues(ChargeTime.ShareAccountActivate, ChargeTime.SharePurchase, ChargeTime.ShareRedeem);
-        this.chargeTimeField = new Select2SingleChoice<>("chargeTimeField", 0, new PropertyModel<>(this, "chargeTimeValue"), this.chargeTimeProvider);
-        this.chargeTimeField.setLabel(Model.of("Charge time type"));
-        this.chargeTimeField.setRequired(true);
-        this.chargeTimeField.add(new OnChangeAjaxBehavior());
-        this.form.add(this.chargeTimeField);
-        this.chargeTimeFeedback = new TextFeedbackPanel("chargeTimeFeedback", this.chargeTimeField);
-        this.form.add(this.chargeTimeFeedback);
+        initChargeTimeBlock();
 
+        initChargeCalculationBlock();
+
+        initAmountBlock();
+
+        initActiveBlock();
+
+        initTaxGroupBlock();
+    }
+
+    protected void initTaxGroupBlock() {
+        this.taxGroupBlock = new WebMarkupBlock("taxGroupBlock", Size.Six_6);
+        this.form.add(this.taxGroupBlock);
+        this.taxGroupIContainer = new WebMarkupContainer("taxGroupIContainer");
+        this.taxGroupBlock.add(this.taxGroupIContainer);
+        this.taxGroupProvider = new SingleChoiceProvider("m_tax_group", "id", "name");
+        this.taxGroupField = new Select2SingleChoice<>("taxGroupField", 0, new PropertyModel<>(this, "taxGroupValue"), this.taxGroupProvider);
+        this.taxGroupField.setLabel(Model.of("Tax Group"));
+        this.taxGroupIContainer.add(this.taxGroupField);
+        this.taxGroupFeedback = new TextFeedbackPanel("taxGroupFeedback", this.taxGroupField);
+        this.taxGroupIContainer.add(this.taxGroupFeedback);
+    }
+
+    protected void initActiveBlock() {
+        this.activeBlock = new WebMarkupBlock("activeBlock", Size.Six_6);
+        this.form.add(this.activeBlock);
+        this.activeIContainer = new WebMarkupContainer("activeIContainer");
+        this.activeBlock.add(this.activeIContainer);
+        this.activeField = new CheckBox("activeField", new PropertyModel<>(this, "activeValue"));
+        this.activeField.setRequired(true);
+        this.activeIContainer.add(this.activeField);
+        this.activeFeedback = new TextFeedbackPanel("activeFeedback", this.activeField);
+        this.activeIContainer.add(this.activeFeedback);
+    }
+
+    protected void initAmountBlock() {
+        this.amountBlock = new WebMarkupBlock("amountBlock", Size.Six_6);
+        this.form.add(this.amountBlock);
+        this.amountIContainer = new WebMarkupContainer("amountIContainer");
+        this.amountBlock.add(this.amountIContainer);
+        this.amountField = new TextField<>("amountField", new PropertyModel<>(this, "amountValue"));
+        this.amountField.setRequired(true);
+        this.amountField.setLabel(Model.of("Amount"));
+        this.amountIContainer.add(this.amountField);
+        this.amountFeedback = new TextFeedbackPanel("amountFeedback", this.amountField);
+        this.amountIContainer.add(this.amountFeedback);
+    }
+
+    protected void initChargeCalculationBlock() {
+        this.chargeCalculationBlock = new WebMarkupBlock("chargeCalculationBlock", Size.Six_6);
+        this.form.add(this.chargeCalculationBlock);
+        this.chargeCalculationIContainer = new WebMarkupContainer("chargeCalculationIContainer");
+        this.chargeCalculationBlock.add(this.chargeCalculationIContainer);
         this.chargeCalculationProvider = new ChargeCalculationProvider();
         this.chargeCalculationProvider.setValues(ChargeCalculation.Flat, ChargeCalculation.ApprovedAmount);
         this.chargeCalculationField = new Select2SingleChoice<>("chargeCalculationField", 0, new PropertyModel<>(this, "chargeCalculationValue"), this.chargeCalculationProvider);
         this.chargeCalculationField.setLabel(Model.of("Charge calculation"));
         this.chargeCalculationField.setRequired(true);
         this.chargeCalculationField.add(new OnChangeAjaxBehavior());
-        this.form.add(this.chargeCalculationField);
+        this.chargeCalculationIContainer.add(this.chargeCalculationField);
         this.chargeCalculationFeedback = new TextFeedbackPanel("chargeCalculationFeedback", this.chargeCalculationField);
-        this.form.add(this.chargeCalculationFeedback);
+        this.chargeCalculationIContainer.add(this.chargeCalculationFeedback);
+    }
 
-        this.amountField = new TextField<>("amountField", new PropertyModel<>(this, "amountValue"));
-        this.amountField.setRequired(true);
-        this.amountField.setLabel(Model.of("Amount"));
-        this.form.add(this.amountField);
-        this.amountFeedback = new TextFeedbackPanel("amountFeedback", this.amountField);
-        this.form.add(this.amountFeedback);
+    protected void initChargeTimeBlock() {
+        this.chargeTimeBlock = new WebMarkupBlock("chargeTimeBlock", Size.Six_6);
+        this.form.add(this.chargeTimeBlock);
+        this.chargeTimeIContainer = new WebMarkupContainer("chargeTimeIContainer");
+        this.chargeTimeBlock.add(this.chargeTimeIContainer);
+        this.chargeTimeProvider = new ChargeTimeProvider();
+        this.chargeTimeProvider.setValues(ChargeTime.ShareAccountActivate, ChargeTime.SharePurchase, ChargeTime.ShareRedeem);
+        this.chargeTimeField = new Select2SingleChoice<>("chargeTimeField", 0, new PropertyModel<>(this, "chargeTimeValue"), this.chargeTimeProvider);
+        this.chargeTimeField.setLabel(Model.of("Charge time type"));
+        this.chargeTimeField.setRequired(true);
+        this.chargeTimeField.add(new OnChangeAjaxBehavior());
+        this.chargeTimeIContainer.add(this.chargeTimeField);
+        this.chargeTimeFeedback = new TextFeedbackPanel("chargeTimeFeedback", this.chargeTimeField);
+        this.chargeTimeIContainer.add(this.chargeTimeFeedback);
+    }
 
-        this.activeField = new CheckBox("activeField", new PropertyModel<>(this, "activeValue"));
-        this.activeField.setRequired(true);
-        this.form.add(this.activeField);
-        this.activeFeedback = new TextFeedbackPanel("activeFeedback", this.activeField);
-        this.form.add(this.activeFeedback);
+    protected void initCurrencyBlock() {
+        this.currencyBlock = new WebMarkupBlock("currencyBlock", Size.Six_6);
+        this.form.add(this.currencyBlock);
+        this.currencyIContainer = new WebMarkupContainer("currencyIContainer");
+        this.currencyBlock.add(this.currencyIContainer);
+        this.currencyProvider = new CurrencyProvider();
+        this.currencyField = new Select2SingleChoice<>("currencyField", 0, new PropertyModel<>(this, "currencyValue"), this.currencyProvider);
+        this.currencyField.setLabel(Model.of("Currency"));
+        this.currencyField.setRequired(true);
+        this.currencyIContainer.add(this.currencyField);
+        this.currencyFeedback = new TextFeedbackPanel("currencyFeedback", this.currencyField);
+        this.currencyIContainer.add(this.currencyFeedback);
+    }
 
-        this.taxGroupProvider = new SingleChoiceProvider("m_tax_group", "id", "name");
-        this.taxGroupField = new Select2SingleChoice<>("taxGroupField", 0, new PropertyModel<>(this, "taxGroupValue"), this.taxGroupProvider);
-        this.taxGroupField.setLabel(Model.of("Tax Group"));
-        this.form.add(this.taxGroupField);
-        this.taxGroupFeedback = new TextFeedbackPanel("taxGroupFeedback", this.taxGroupField);
-        this.form.add(this.taxGroupFeedback);
+    protected void initNameBlock() {
+        this.nameBlock = new WebMarkupBlock("nameBlock", Size.Six_6);
+        this.form.add(this.nameBlock);
+        this.nameIContainer = new WebMarkupContainer("nameIContainer");
+        this.nameBlock.add(this.nameIContainer);
+        this.nameField = new TextField<>("nameField", new PropertyModel<>(this, "nameValue"));
+        this.nameField.setLabel(Model.of("Name"));
+        this.nameField.setRequired(true);
+        this.nameIContainer.add(this.nameField);
+        this.nameFeedback = new TextFeedbackPanel("nameFeedback", this.nameField);
+        this.nameIContainer.add(this.nameFeedback);
+    }
 
+    @Override
+    protected void configureRequiredValidation() {
+    }
+
+    @Override
+    protected void configureMetaData() {
     }
 
     protected void saveButtonSubmit(Button button) {
