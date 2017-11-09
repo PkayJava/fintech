b4ac50c19376065563a6e140df29bba836102faf
Socheat KHAUV <pkayjava@gmail.com>
2017-11-09 22:45:46 +0800

    office browse page

--- src/main/java/com/angkorteam/fintech/pages/office/OfficeBrowsePage.java
+++ src/main/java/com/angkorteam/fintech/pages/office/OfficeBrowsePage.java
@@ -4,21 +4,24 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.FilterForm;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-import com.angkorteam.fintech.DeprecatedPage;
-import com.angkorteam.fintech.DeprecatedPage;
+import com.angkorteam.fintech.Page;
 import com.angkorteam.fintech.dto.Function;
 import com.angkorteam.fintech.pages.OrganizationDashboardPage;
 import com.angkorteam.fintech.provider.JdbcProvider;
 import com.angkorteam.fintech.table.LinkCell;
 import com.angkorteam.fintech.table.TextCell;
+import com.angkorteam.fintech.widget.WebMarkupBlock;
+import com.angkorteam.fintech.widget.WebMarkupBlock.Size;
 import com.angkorteam.framework.models.PageBreadcrumb;
 import com.angkorteam.framework.wicket.extensions.markup.html.repeater.data.table.DataTable;
 import com.angkorteam.framework.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
@@ -34,17 +37,20 @@ import com.google.common.collect.Lists;
  * Created by socheatkhauv on 6/22/17.
  */
 @AuthorizeInstantiation(Function.ALL_FUNCTION)
-public class OfficeBrowsePage extends DeprecatedPage {
+public class OfficeBrowsePage extends Page {
 
-    private DataTable<Map<String, Object>, String> dataTable;
+    protected WebMarkupBlock dataBlock;
+    protected WebMarkupContainer dataIContainer;
+    protected DataTable<Map<String, Object>, String> dataTable;
+    protected JdbcProvider dataProvider;
+    protected List<IColumn<Map<String, Object>, String>> dataColumn;
+    protected FilterForm<Map<String, String>> dataFilterForm;
 
-    private JdbcProvider provider;
+    protected BookmarkablePageLink<Void> hierarchyLink;
 
-    private BookmarkablePageLink<Void> hierarchyLink;
+    protected BookmarkablePageLink<Void> createLink;
 
-    private BookmarkablePageLink<Void> createLink;
-
-    private static final List<PageBreadcrumb> BREADCRUMB;
+    protected static final List<PageBreadcrumb> BREADCRUMB;
 
     @Override
     public IModel<List<PageBreadcrumb>> buildPageBreadcrumb() {
@@ -72,72 +78,86 @@ public class OfficeBrowsePage extends DeprecatedPage {
     }
 
     @Override
-    protected void onInitialize() {
-        super.onInitialize();
-        this.provider = new JdbcProvider("m_office office");
-        this.provider.addJoin("LEFT JOIN m_office parent ON office.parent_id = parent.id");
-        this.provider.boardField("office.id", "id", Long.class);
-        this.provider.boardField("office.external_id", "external_id", String.class);
-        this.provider.boardField("office.parent_id", "parent_id", Long.class);
-        this.provider.boardField("office.name", "name", String.class);
-        this.provider.boardField("parent.name", "parent_name", String.class);
-        this.provider.boardField("office.opening_date", "opening_date", Calendar.Date);
+    protected void initData() {
+    }
 
-        this.provider.selectField("parent_id", Long.class);
-
-        List<IColumn<Map<String, Object>, String>> columns = Lists.newArrayList();
-        columns.add(new TextFilterColumn(this.provider, ItemClass.Long, Model.of("ID"), "id", "id", this::idColumn));
-        columns.add(new TextFilterColumn(this.provider, ItemClass.String, Model.of("External ID"), "external_id", "external_id", this::externalIdColumn));
-        columns.add(new TextFilterColumn(this.provider, ItemClass.String, Model.of("Name"), "name", "name", this::nameColumn));
-        columns.add(new TextFilterColumn(this.provider, ItemClass.String, Model.of("Parent Name"), "parent_name", "parent_name", this::parentNameColumn));
-        columns.add(new TextFilterColumn(this.provider, ItemClass.Date, Model.of("Opening Date"), "opening_date", "opening_date", this::openingDateColumn));
-
-        FilterForm<Map<String, String>> filterForm = new FilterForm<>("filter-form", this.provider);
-        add(filterForm);
-
-        this.dataTable = new DefaultDataTable<>("table", columns, this.provider, 20);
-        this.dataTable.addTopToolbar(new FilterToolbar(this.dataTable, filterForm));
-        filterForm.add(this.dataTable);
+    @Override
+    protected void initComponent() {
 
         this.hierarchyLink = new BookmarkablePageLink<>("hierarchyLink", OfficeHierarchyPage.class);
         add(this.hierarchyLink);
 
         this.createLink = new BookmarkablePageLink<>("createLink", OfficeCreatePage.class);
         add(this.createLink);
+
+        initDataBlock();
     }
 
-    private ItemPanel idColumn(String jdbcColumn, IModel<String> display, Map<String, Object> model) {
-        Long value = (Long) model.get(jdbcColumn);
-        return new TextCell(value);
+    protected void initDataBlock() {
+        this.dataBlock = new WebMarkupBlock("dataBlock", Size.Twelve_12);
+        add(this.dataBlock);
+        this.dataIContainer = new WebMarkupContainer("dataIContainer");
+        this.dataBlock.add(this.dataIContainer);
+        this.dataProvider = new JdbcProvider("m_office office");
+        this.dataProvider.addJoin("LEFT JOIN m_office parent ON office.parent_id = parent.id");
+        this.dataProvider.boardField("office.id", "id", Long.class);
+        this.dataProvider.boardField("office.external_id", "external_id", String.class);
+        this.dataProvider.boardField("office.parent_id", "parent_id", Long.class);
+        this.dataProvider.boardField("office.name", "name", String.class);
+        this.dataProvider.boardField("parent.name", "parent_name", String.class);
+        this.dataProvider.boardField("office.opening_date", "opening_date", Calendar.Date);
+
+        this.dataProvider.selectField("parent_id", Long.class);
+
+        this.dataColumn = Lists.newArrayList();
+        this.dataColumn.add(new TextFilterColumn(this.dataProvider, ItemClass.Long, Model.of("ID"), "id", "id", this::dataColumn));
+        this.dataColumn.add(new TextFilterColumn(this.dataProvider, ItemClass.String, Model.of("External ID"), "external_id", "external_id", this::dataColumn));
+        this.dataColumn.add(new TextFilterColumn(this.dataProvider, ItemClass.String, Model.of("Name"), "name", "name", this::dataColumn));
+        this.dataColumn.add(new TextFilterColumn(this.dataProvider, ItemClass.String, Model.of("Parent Name"), "parent_name", "parent_name", this::dataColumn));
+        this.dataColumn.add(new TextFilterColumn(this.dataProvider, ItemClass.Date, Model.of("Opening Date"), "opening_date", "opening_date", this::dataColumn));
+
+        this.dataFilterForm = new FilterForm<>("dataFilterForm", this.dataProvider);
+        this.dataIContainer.add(this.dataFilterForm);
+
+        this.dataTable = new DefaultDataTable<>("dataTable", this.dataColumn, this.dataProvider, 20);
+        this.dataTable.addTopToolbar(new FilterToolbar(this.dataTable, this.dataFilterForm));
+        this.dataFilterForm.add(this.dataTable);
     }
 
-    private ItemPanel externalIdColumn(String jdbcColumn, IModel<String> display, Map<String, Object> model) {
-        String value = (String) model.get(jdbcColumn);
-        return new TextCell(value);
+    @Override
+    protected void configureRequiredValidation() {
     }
 
-    private ItemPanel parentNameColumn(String jdbcColumn, IModel<String> display, Map<String, Object> model) {
-        String value = (String) model.get(jdbcColumn);
-        if (Strings.isNullOrEmpty(value)) {
+    @Override
+    protected void configureMetaData() {
+    }
+
+    protected ItemPanel dataColumn(String column, IModel<String> display, Map<String, Object> model) {
+        if ("id".equals(column)) {
+            Long value = (Long) model.get(column);
             return new TextCell(value);
-        } else {
+        } else if ("external_id".equals(column)) {
+            String value = (String) model.get(column);
+            return new TextCell(value);
+        } else if ("name".equals(column)) {
+            String name = (String) model.get(column);
             PageParameters parameters = new PageParameters();
-            parameters.add("officeId", model.get("parent_id"));
-            return new LinkCell(OfficeModifyPage.class, parameters, Model.of(value));
+            parameters.add("officeId", model.get("id"));
+            return new LinkCell(OfficeModifyPage.class, parameters, name);
+        } else if ("parent_name".equals(column)) {
+            String value = (String) model.get(column);
+            if (Strings.isNullOrEmpty(value)) {
+                return new TextCell(value);
+            } else {
+                PageParameters parameters = new PageParameters();
+                parameters.add("officeId", model.get("parent_id"));
+                return new LinkCell(OfficeModifyPage.class, parameters, value);
+            }
+        } else if ("opening_date".equals(column)) {
+            Date value = (Date) model.get(column);
+            return new TextCell(value, "dd/MM/yyyy");
         }
-
-    }
-
-    private ItemPanel nameColumn(String jdbcColumn, IModel<String> display, Map<String, Object> model) {
-        String name = (String) model.get(jdbcColumn);
-        PageParameters parameters = new PageParameters();
-        parameters.add("officeId", model.get("id"));
-        return new LinkCell(OfficeModifyPage.class, parameters, Model.of(name));
-    }
-
-    private ItemPanel openingDateColumn(String jdbcColumn, IModel<String> display, Map<String, Object> model) {
-        Date value = (Date) model.get(jdbcColumn);
-        return new TextCell(value, "dd/MM/yyyy");
+        throw new WicketRuntimeException("Unknown " + column);
     }
 
 }
