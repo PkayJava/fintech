e8730510ee56c95eea1630213af0509e320b89cd
Socheat KHAUV <pkayjava@gmail.com>
2017-11-09 22:13:16 +0800

    user create page

--- src/main/java/com/angkorteam/fintech/pages/staff/UserCreatePage.java
+++ src/main/java/com/angkorteam/fintech/pages/staff/UserCreatePage.java
@@ -14,7 +14,7 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 
-import com.angkorteam.fintech.DeprecatedPage;
+import com.angkorteam.fintech.Page;
 import com.angkorteam.fintech.Session;
 import com.angkorteam.fintech.dto.Function;
 import com.angkorteam.fintech.dto.builder.AppUserBuilder;
@@ -36,7 +36,7 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 @AuthorizeInstantiation(Function.ALL_FUNCTION)
-public class UserCreatePage extends DeprecatedPage {
+public class UserCreatePage extends Page {
 
     protected Form<Void> form;
     protected Button saveButton;
@@ -149,6 +149,8 @@ public class UserCreatePage extends DeprecatedPage {
         this.form.add(this.closeLink);
 
         initFirstNameBlock();
+
+        initPasswordBlock();
 
         initRepeatPasswordBlock();
 
