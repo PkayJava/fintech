42e2b4d0f6dbde307cfd9938d65ffacd9905f135
Socheat KHAUV <pkayjava@gmail.com>
2017-11-11 14:45:03 +0800

    CheckerBrowsePage

--- src/main/java/com/angkorteam/fintech/pages/entity/CheckerBrowsePage.java
+++ src/main/java/com/angkorteam/fintech/pages/entity/CheckerBrowsePage.java
@@ -4,16 +4,17 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.FilterForm;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
-import com.angkorteam.fintech.DeprecatedPage;
-import com.angkorteam.fintech.DeprecatedPage;
+import com.angkorteam.fintech.Page;
 import com.angkorteam.fintech.Session;
 import com.angkorteam.fintech.dto.Function;
 import com.angkorteam.fintech.dto.enums.EntityStatus;
@@ -23,6 +24,8 @@ import com.angkorteam.fintech.pages.OrganizationDashboardPage;
 import com.angkorteam.fintech.provider.JdbcProvider;
 import com.angkorteam.fintech.table.BadgeCell;
 import com.angkorteam.fintech.table.TextCell;
+import com.angkorteam.fintech.widget.WebMarkupBlock;
+import com.angkorteam.fintech.widget.WebMarkupBlock.Size;
 import com.angkorteam.framework.BadgeType;
 import com.angkorteam.framework.models.PageBreadcrumb;
 import com.angkorteam.framework.wicket.extensions.markup.html.repeater.data.table.DataTable;
@@ -42,15 +45,18 @@ import com.mashape.unirest.http.exceptions.UnirestException;
  * Created by socheatkhauv on 7/15/17.
  */
 @AuthorizeInstantiation(Function.ALL_FUNCTION)
-public class CheckerBrowsePage extends DeprecatedPage {
+public class CheckerBrowsePage extends Page {
 
-    private DataTable<Map<String, Object>, String> dataTable;
+    protected WebMarkupBlock dataBlock;
+    protected WebMarkupContainer dataIContainer;
+    protected DataTable<Map<String, Object>, String> dataTable;
+    protected JdbcProvider dataProvider;
+    protected List<IColumn<Map<String, Object>, String>> dataColumn;
+    protected FilterForm<Map<String, String>> dataFilterForm;
 
-    private JdbcProvider provider;
+    protected BookmarkablePageLink<Void> createLink;
 
-    private BookmarkablePageLink<Void> createLink;
-
-    private static final List<PageBreadcrumb> BREADCRUMB;
+    protected static final List<PageBreadcrumb> BREADCRUMB;
 
     @Override
     public IModel<List<PageBreadcrumb>> buildPageBreadcrumb() {
@@ -78,8 +84,26 @@ public class CheckerBrowsePage extends DeprecatedPage {
     }
 
     @Override
-    protected void onInitialize() {
-        super.onInitialize();
+    protected void initData() {
+    }
+
+    @Override
+    protected void initComponent() {
+        initDataBlock();
+
+        this.createLink = new BookmarkablePageLink<>("createLink", CheckerCreatePage.class);
+        add(this.createLink);
+    }
+
+    @Override
+    protected void configureRequiredValidation() {
+    }
+
+    @Override
+    protected void configureMetaData() {
+    }
+
+    protected void initDataBlock() {
         List<String> entity = Lists.newArrayList();
         for (EntityType t : EntityType.values()) {
             entity.add("when '" + t.getLiteral() + "' then '" + t.getDescription() + "'");
@@ -91,36 +115,37 @@ public class CheckerBrowsePage extends DeprecatedPage {
         }
         status.add("else 'N/A'");
 
-        this.provider = new JdbcProvider("m_entity_datatable_check");
-        this.provider.boardField("id", "id", Long.class);
-        this.provider.boardField("case application_table_name " + StringUtils.join(entity, " ") + " end", "entity", String.class);
-        this.provider.boardField("case system_defined when 0 then 'No' else 'Yes' end", "system", String.class);
-        this.provider.boardField("case status_enum " + StringUtils.join(status, " ") + " end", "status", String.class);
-        this.provider.boardField("x_registered_table_name", "dataTable", String.class);
-        this.provider.boardField("product_id", "product", Long.class);
+        this.dataBlock = new WebMarkupBlock("dataBlock", Size.Twelve_12);
+        add(this.dataBlock);
+        this.dataIContainer = new WebMarkupContainer("dataIContainer");
+        this.dataBlock.add(this.dataIContainer);
+        this.dataProvider = new JdbcProvider("m_entity_datatable_check");
+        this.dataProvider.boardField("id", "id", Long.class);
+        this.dataProvider.boardField("case application_table_name " + StringUtils.join(entity, " ") + " end", "entity", String.class);
+        this.dataProvider.boardField("case system_defined when 0 then 'No' else 'Yes' end", "system", String.class);
+        this.dataProvider.boardField("case status_enum " + StringUtils.join(status, " ") + " end", "status", String.class);
+        this.dataProvider.boardField("x_registered_table_name", "dataTable", String.class);
+        this.dataProvider.boardField("product_id", "product", Long.class);
 
-        this.provider.selectField("id", Long.class);
+        this.dataProvider.selectField("id", Long.class);
 
-        List<IColumn<Map<String, Object>, String>> columns = Lists.newArrayList();
-        columns.add(new TextFilterColumn(this.provider, ItemClass.String, Model.of("Entity"), "entity", "entity", this::entityColumn));
-        columns.add(new TextFilterColumn(this.provider, ItemClass.Long, Model.of("Product"), "product", "account", this::productColumn));
-        columns.add(new TextFilterColumn(this.provider, ItemClass.String, Model.of("Data Table"), "dataTable", "dataTable", this::dataTableColumn));
-        columns.add(new TextFilterColumn(this.provider, ItemClass.String, Model.of("Status"), "status", "status", this::statusColumn));
-        columns.add(new TextFilterColumn(this.provider, ItemClass.String, Model.of("System"), "system", "system", this::systemColumn));
-        columns.add(new ActionFilterColumn<>(Model.of("Action"), this::actionItem, this::actionClick));
+        this.dataColumn = Lists.newArrayList();
+        this.dataColumn.add(new TextFilterColumn(this.dataProvider, ItemClass.String, Model.of("Entity"), "entity", "entity", this::dataColumn));
+        this.dataColumn.add(new TextFilterColumn(this.dataProvider, ItemClass.Long, Model.of("Product"), "product", "account", this::dataColumn));
+        this.dataColumn.add(new TextFilterColumn(this.dataProvider, ItemClass.String, Model.of("Data Table"), "dataTable", "dataTable", this::dataColumn));
+        this.dataColumn.add(new TextFilterColumn(this.dataProvider, ItemClass.String, Model.of("Status"), "status", "status", this::dataColumn));
+        this.dataColumn.add(new TextFilterColumn(this.dataProvider, ItemClass.String, Model.of("System"), "system", "system", this::dataColumn));
+        this.dataColumn.add(new ActionFilterColumn<>(Model.of("Action"), this::dataAction, this::dataClick));
 
-        FilterForm<Map<String, String>> filterForm = new FilterForm<>("filter-form", this.provider);
-        add(filterForm);
+        this.dataFilterForm = new FilterForm<>("dataFilterForm", this.dataProvider);
+        this.dataIContainer.add(this.dataFilterForm);
 
-        this.dataTable = new DefaultDataTable<>("table", columns, this.provider, 20);
-        this.dataTable.addTopToolbar(new FilterToolbar(this.dataTable, filterForm));
-        filterForm.add(this.dataTable);
-
-        this.createLink = new BookmarkablePageLink<>("createLink", CheckerCreatePage.class);
-        add(this.createLink);
+        this.dataTable = new DefaultDataTable<>("dataTable", this.dataColumn, this.dataProvider, 20);
+        this.dataTable.addTopToolbar(new FilterToolbar(this.dataTable, this.dataFilterForm));
+        this.dataFilterForm.add(this.dataTable);
     }
 
-    private void actionClick(String s, Map<String, Object> model, AjaxRequestTarget target) {
+    protected void dataClick(String s, Map<String, Object> model, AjaxRequestTarget target) {
         Integer id = (Integer) model.get("id");
         JsonNode node = null;
         try {
@@ -131,41 +156,30 @@ public class CheckerBrowsePage extends DeprecatedPage {
         target.add(this.dataTable);
     }
 
-    private List<ActionItem> actionItem(String s, Map<String, Object> model) {
+    protected List<ActionItem> dataAction(String s, Map<String, Object> model) {
         List<ActionItem> actions = Lists.newArrayList();
         actions.add(new ActionItem("delete", Model.of("Delete"), ItemCss.DANGER));
         return actions;
     }
 
-    private ItemPanel productColumn(String jdbcColumn, IModel<String> display, Map<String, Object> model) {
-        Integer value = (Integer) model.get(jdbcColumn);
-        return new TextCell(value);
-    }
-
-    private ItemPanel entityColumn(String jdbcColumn, IModel<String> display, Map<String, Object> model) {
-        String value = (String) model.get(jdbcColumn);
-        return new TextCell(value);
-    }
-
-    private ItemPanel dataTableColumn(String jdbcColumn, IModel<String> display, Map<String, Object> model) {
-        String value = (String) model.get(jdbcColumn);
-        return new TextCell(value);
-    }
-
-    private ItemPanel systemColumn(String jdbcColumn, IModel<String> display, Map<String, Object> model) {
-        String value = (String) model.get(jdbcColumn);
-        if ("Yes".equals(value)) {
-            return new BadgeCell(BadgeType.Danger, Model.of("Yes"));
-        } else if ("No".equals(value)) {
-            return new BadgeCell(BadgeType.Success, Model.of("No"));
-        } else {
+    protected ItemPanel dataColumn(String column, IModel<String> display, Map<String, Object> model) {
+        if ("entity".equals(column) || "dataTable".equals(column) || "status".equals(column)) {
+            String value = (String) model.get(column);
             return new TextCell(value);
+        } else if ("account".equals(column)) {
+            Integer value = (Integer) model.get(column);
+            return new TextCell(value);
+        } else if ("system".equals(column)) {
+            String value = (String) model.get(column);
+            if ("Yes".equals(value)) {
+                return new BadgeCell(BadgeType.Danger, Model.of("Yes"));
+            } else if ("No".equals(value)) {
+                return new BadgeCell(BadgeType.Success, Model.of("No"));
+            } else {
+                return new TextCell(value);
+            }
         }
-    }
-
-    private ItemPanel statusColumn(String jdbcColumn, IModel<String> display, Map<String, Object> model) {
-        String value = (String) model.get(jdbcColumn);
-        return new TextCell(value);
+        throw new WicketRuntimeException("Unknown " + column);
     }
 
 }
