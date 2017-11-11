77884b79e389f8adc23becce50be262f39b677d8
Socheat KHAUV <pkayjava@gmail.com>
2017-11-11 14:08:51 +0800

    ValueBrowsePage

--- src/main/java/com/angkorteam/fintech/pages/client/center/ChargePreviewPage.java
+++ src/main/java/com/angkorteam/fintech/pages/client/center/ChargePreviewPage.java
@@ -1,7 +1,6 @@
 package com.angkorteam.fintech.pages.client.center;
 
 import com.angkorteam.fintech.DeprecatedPage;
-import com.angkorteam.fintech.DeprecatedPage;
 
 public class ChargePreviewPage extends DeprecatedPage {
 
--- src/main/java/com/angkorteam/fintech/pages/code/ValueBrowsePage.java
+++ src/main/java/com/angkorteam/fintech/pages/code/ValueBrowsePage.java
@@ -3,11 +3,13 @@ package com.angkorteam.fintech.pages.code;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.FilterForm;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.IModel;
@@ -15,8 +17,7 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-import com.angkorteam.fintech.DeprecatedPage;
-import com.angkorteam.fintech.DeprecatedPage;
+import com.angkorteam.fintech.Page;
 import com.angkorteam.fintech.Session;
 import com.angkorteam.fintech.dto.Function;
 import com.angkorteam.fintech.dto.builder.CodeValueBuilder;
@@ -26,6 +27,8 @@ import com.angkorteam.fintech.provider.JdbcProvider;
 import com.angkorteam.fintech.table.BadgeCell;
 import com.angkorteam.fintech.table.TextCell;
 import com.angkorteam.fintech.widget.TextFeedbackPanel;
+import com.angkorteam.fintech.widget.WebMarkupBlock;
+import com.angkorteam.fintech.widget.WebMarkupBlock.Size;
 import com.angkorteam.framework.BadgeType;
 import com.angkorteam.framework.SpringBean;
 import com.angkorteam.framework.models.PageBreadcrumb;
@@ -49,26 +52,37 @@ import com.mashape.unirest.http.exceptions.UnirestException;
  * Created by socheatkhauv on 6/27/17.
  */
 @AuthorizeInstantiation(Function.ALL_FUNCTION)
-public class ValueBrowsePage extends DeprecatedPage {
-
-    protected DataTable<Map<String, Object>, String> dataTable;
+public class ValueBrowsePage extends Page {
 
     protected String codeId;
 
-    protected JdbcProvider provider;
+    protected WebMarkupBlock dataBlock;
+    protected WebMarkupContainer dataIContainer;
+    protected DataTable<Map<String, Object>, String> dataTable;
+    protected JdbcProvider dataProvider;
+    protected List<IColumn<Map<String, Object>, String>> dataColumn;
+    protected FilterForm<Map<String, String>> dataFilterForm;
 
+    protected WebMarkupBlock nameBlock;
+    protected WebMarkupContainer nameIContainer;
     protected String nameValue;
     protected TextField<String> nameField;
     protected TextFeedbackPanel nameFeedback;
 
+    protected WebMarkupBlock descriptionBlock;
+    protected WebMarkupContainer descriptionIContainer;
     protected String descriptionValue;
     protected TextField<String> descriptionField;
     protected TextFeedbackPanel descriptionFeedback;
 
+    protected WebMarkupBlock positionBlock;
+    protected WebMarkupContainer positionIContainer;
     protected Integer positionValue;
     protected TextField<Integer> positionField;
     protected TextFeedbackPanel positionFeedback;
 
+    protected WebMarkupBlock activeBlock;
+    protected WebMarkupContainer activeIContainer;
     protected Boolean activeValue;
     protected CheckBox activeField;
     protected TextFeedbackPanel activeFeedback;
@@ -116,37 +130,15 @@ public class ValueBrowsePage extends DeprecatedPage {
     }
 
     @Override
-    protected void onInitialize() {
-        super.onInitialize();
-
+    protected void initData() {
         PageParameters parameters = getPageParameters();
         this.codeId = parameters.get("codeId").toString("");
+    }
 
-        this.provider = new JdbcProvider("m_code_value");
-        this.provider.applyWhere("code", "code_id = " + this.codeId);
-        this.provider.boardField("id", "id", Integer.class);
-        this.provider.boardField("code_value", "code_value", String.class);
-        this.provider.boardField("code_description", "code_description", String.class);
-        this.provider.boardField("order_position", "order_position", Integer.class);
-        this.provider.boardField("is_active", "active", Boolean.class);
+    @Override
+    protected void initComponent() {
 
-        this.provider.setSort("code_value", SortOrder.ASCENDING);
-
-        this.provider.selectField("id", Integer.class);
-
-        List<IColumn<Map<String, Object>, String>> columns = Lists.newArrayList();
-        columns.add(new TextFilterColumn(this.provider, ItemClass.String, Model.of("Name"), "code_value", "code_value", this::nameColumn));
-        columns.add(new TextFilterColumn(this.provider, ItemClass.String, Model.of("Description"), "code_description", "code_description", this::descriptionColumn));
-        columns.add(new TextFilterColumn(this.provider, ItemClass.Integer, Model.of("Position"), "order_position", "order_position", this::positionColumn));
-        columns.add(new TextFilterColumn(this.provider, ItemClass.Boolean, Model.of("Is Active ?"), "active", "active", this::activeColumn));
-        columns.add(new ActionFilterColumn<>(Model.of("Action"), this::actionItem, this::actionClick));
-
-        FilterForm<Map<String, String>> filterForm = new FilterForm<>("filter-form", this.provider);
-        add(filterForm);
-
-        this.dataTable = new DefaultDataTable<>("table", columns, this.provider, 20);
-        this.dataTable.addTopToolbar(new FilterToolbar(this.dataTable, filterForm));
-        filterForm.add(this.dataTable);
+        initDataBlock();
 
         this.form = new Form<>("form");
         add(this.form);
@@ -155,32 +147,106 @@ public class ValueBrowsePage extends DeprecatedPage {
         this.addButton.setOnSubmit(this::addButtonSubmit);
         this.form.add(this.addButton);
 
-        this.nameField = new TextField<>("nameField", new PropertyModel<>(this, "nameValue"));
-        this.nameField.setLabel(Model.of("Name"));
-        this.nameField.setRequired(true);
-        this.form.add(this.nameField);
-        this.nameFeedback = new TextFeedbackPanel("nameFeedback", this.nameField);
-        this.form.add(this.nameFeedback);
+        initNameBlock();
 
-        this.descriptionField = new TextField<>("descriptionField", new PropertyModel<>(this, "descriptionValue"));
-        this.descriptionField.setLabel(Model.of("Description"));
-        this.descriptionField.setRequired(true);
-        this.form.add(this.descriptionField);
-        this.descriptionFeedback = new TextFeedbackPanel("descriptionFeedback", this.descriptionField);
-        this.form.add(this.descriptionFeedback);
+        initDescriptionBlock();
 
+        initPositionBlock();
+
+        initActiveBlock();
+    }
+
+    @Override
+    protected void configureRequiredValidation() {
+
+    }
+
+    @Override
+    protected void configureMetaData() {
+
+    }
+
+    protected void initDataBlock() {
+        this.dataBlock = new WebMarkupBlock("dataBlock", Size.Twelve_12);
+        add(this.dataBlock);
+        this.dataIContainer = new WebMarkupContainer("dataIContainer");
+        this.dataBlock.add(this.dataIContainer);
+        this.dataProvider = new JdbcProvider("m_code_value");
+        this.dataProvider.applyWhere("code", "code_id = " + this.codeId);
+        this.dataProvider.boardField("id", "id", Integer.class);
+        this.dataProvider.boardField("code_value", "code_value", String.class);
+        this.dataProvider.boardField("code_description", "code_description", String.class);
+        this.dataProvider.boardField("order_position", "order_position", Integer.class);
+        this.dataProvider.boardField("is_active", "active", Boolean.class);
+
+        this.dataProvider.setSort("code_value", SortOrder.ASCENDING);
+
+        this.dataProvider.selectField("id", Integer.class);
+
+        this.dataColumn = Lists.newArrayList();
+        this.dataColumn.add(new TextFilterColumn(this.dataProvider, ItemClass.String, Model.of("Name"), "code_value", "code_value", this::dataColumn));
+        this.dataColumn.add(new TextFilterColumn(this.dataProvider, ItemClass.String, Model.of("Description"), "code_description", "code_description", this::dataColumn));
+        this.dataColumn.add(new TextFilterColumn(this.dataProvider, ItemClass.Integer, Model.of("Position"), "order_position", "order_position", this::dataColumn));
+        this.dataColumn.add(new TextFilterColumn(this.dataProvider, ItemClass.Boolean, Model.of("Is Active ?"), "active", "active", this::dataColumn));
+        this.dataColumn.add(new ActionFilterColumn<>(Model.of("Action"), this::dataAction, this::dataClick));
+
+        this.dataFilterForm = new FilterForm<>("dataFilterForm", this.dataProvider);
+        this.dataIContainer.add(this.dataFilterForm);
+
+        this.dataTable = new DefaultDataTable<>("dataTable", this.dataColumn, this.dataProvider, 20);
+        this.dataTable.addTopToolbar(new FilterToolbar(this.dataTable, this.dataFilterForm));
+        this.dataFilterForm.add(this.dataTable);
+    }
+
+    protected void initActiveBlock() {
+        this.activeBlock = new WebMarkupBlock("activeBlock", Size.Six_6);
+        this.form.add(this.activeBlock);
+        this.activeIContainer = new WebMarkupContainer("activeIContainer");
+        this.activeBlock.add(this.activeIContainer);
+        this.activeField = new CheckBox("activeField", new PropertyModel<>(this, "activeValue"));
+        this.activeField.setRequired(true);
+        this.activeIContainer.add(this.activeField);
+        this.activeFeedback = new TextFeedbackPanel("activeFeedback", this.activeField);
+        this.activeIContainer.add(this.activeFeedback);
+    }
+
+    protected void initPositionBlock() {
+        this.positionBlock = new WebMarkupBlock("positionBlock", Size.Six_6);
+        this.form.add(this.positionBlock);
+        this.positionIContainer = new WebMarkupContainer("positionIContainer");
+        this.positionBlock.add(this.positionIContainer);
         this.positionField = new TextField<>("positionField", new PropertyModel<>(this, "positionValue"));
         this.positionField.setLabel(Model.of("Position"));
         this.positionField.setRequired(true);
-        this.form.add(this.positionField);
+        this.positionIContainer.add(this.positionField);
         this.positionFeedback = new TextFeedbackPanel("positionFeedback", this.positionField);
-        this.form.add(this.positionFeedback);
+        this.positionIContainer.add(this.positionFeedback);
+    }
 
-        this.activeField = new CheckBox("activeField", new PropertyModel<>(this, "activeValue"));
-        this.activeField.setRequired(true);
-        this.form.add(this.activeField);
-        this.activeFeedback = new TextFeedbackPanel("activeFeedback", this.activeField);
-        this.form.add(this.activeFeedback);
+    protected void initDescriptionBlock() {
+        this.descriptionBlock = new WebMarkupBlock("descriptionBlock", Size.Six_6);
+        this.form.add(this.descriptionBlock);
+        this.descriptionIContainer = new WebMarkupContainer("descriptionIContainer");
+        this.descriptionBlock.add(this.descriptionIContainer);
+        this.descriptionField = new TextField<>("descriptionField", new PropertyModel<>(this, "descriptionValue"));
+        this.descriptionField.setLabel(Model.of("Description"));
+        this.descriptionField.setRequired(true);
+        this.descriptionIContainer.add(this.descriptionField);
+        this.descriptionFeedback = new TextFeedbackPanel("descriptionFeedback", this.descriptionField);
+        this.descriptionIContainer.add(this.descriptionFeedback);
+    }
+
+    protected void initNameBlock() {
+        this.nameBlock = new WebMarkupBlock("nameBlock", Size.Six_6);
+        this.form.add(this.nameBlock);
+        this.nameIContainer = new WebMarkupContainer("nameIContainer");
+        this.nameBlock.add(this.nameIContainer);
+        this.nameField = new TextField<>("nameField", new PropertyModel<>(this, "nameValue"));
+        this.nameField.setLabel(Model.of("Name"));
+        this.nameField.setRequired(true);
+        this.nameIContainer.add(this.nameField);
+        this.nameFeedback = new TextFeedbackPanel("nameFeedback", this.nameField);
+        this.nameIContainer.add(this.nameFeedback);
     }
 
     protected void addButtonSubmit(Button button) {
@@ -208,7 +274,7 @@ public class ValueBrowsePage extends DeprecatedPage {
         setResponsePage(ValueBrowsePage.class, parameters);
     }
 
-    protected void actionClick(String s, Map<String, Object> model, AjaxRequestTarget target) {
+    protected void dataClick(String s, Map<String, Object> model, AjaxRequestTarget target) {
         Integer id = (Integer) model.get("id");
         try {
             if ("disable".equals(s)) {
@@ -229,7 +295,7 @@ public class ValueBrowsePage extends DeprecatedPage {
         target.add(this.dataTable);
     }
 
-    protected List<ActionItem> actionItem(String s, Map<String, Object> model) {
+    protected List<ActionItem> dataAction(String s, Map<String, Object> model) {
         List<ActionItem> actions = Lists.newArrayList();
         Boolean active = (Boolean) model.get("active");
         if (active != null && active) {
@@ -240,28 +306,23 @@ public class ValueBrowsePage extends DeprecatedPage {
         return actions;
     }
 
-    protected ItemPanel nameColumn(String jdbcColumn, IModel<String> display, Map<String, Object> model) {
-        String value = (String) model.get(jdbcColumn);
-        return new TextCell(value);
-    }
+    protected ItemPanel dataColumn(String column, IModel<String> display, Map<String, Object> model) {
+        if ("code_value".equals(column) || "code_description".equals(column)) {
+            String value = (String) model.get(column);
+            return new TextCell(value);
+        } else if ("order_position".equals(column)) {
+            Long value = (Long) model.get(column);
+            return new TextCell(value);
+        } else if ("active".equals(column)) {
+            Boolean value = (Boolean) model.get(column);
+            if (value != null && value) {
+                return new BadgeCell(BadgeType.Success, Model.of("Yes"));
+            } else {
+                return new BadgeCell(BadgeType.Danger, Model.of("No"));
+            }
 
-    protected ItemPanel descriptionColumn(String jdbcColumn, IModel<String> display, Map<String, Object> model) {
-        String value = (String) model.get(jdbcColumn);
-        return new TextCell(value);
-    }
-
-    protected ItemPanel positionColumn(String jdbcColumn, IModel<String> display, Map<String, Object> model) {
-        Integer value = (Integer) model.get(jdbcColumn);
-        return new TextCell(value);
-    }
-
-    protected ItemPanel activeColumn(String jdbcColumn, IModel<String> display, Map<String, Object> model) {
-        Boolean active = (Boolean) model.get(jdbcColumn);
-        if (active != null && active) {
-            return new BadgeCell(BadgeType.Success, Model.of("Yes"));
-        } else {
-            return new BadgeCell(BadgeType.Danger, Model.of("No"));
         }
+        throw new WicketRuntimeException("Unknown " + column);
     }
 
 }
