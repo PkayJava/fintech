c1ef839143c9f5c80644213aefa9426626a0e8a1
Socheat KHAUV <pkayjava@gmail.com>
2017-11-12 00:18:16 +0800

    Payment Type

--- src/main/java/com/angkorteam/fintech/pages/payment/PaymentTypeCreatePage.java
+++ src/main/java/com/angkorteam/fintech/pages/payment/PaymentTypeCreatePage.java
@@ -3,6 +3,7 @@ package com.angkorteam.fintech.pages.payment;
 import java.util.List;
 
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
@@ -10,14 +11,15 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 
-import com.angkorteam.fintech.DeprecatedPage;
-import com.angkorteam.fintech.DeprecatedPage;
+import com.angkorteam.fintech.Page;
 import com.angkorteam.fintech.Session;
 import com.angkorteam.fintech.dto.Function;
 import com.angkorteam.fintech.dto.builder.PaymentTypeBuilder;
 import com.angkorteam.fintech.helper.PaymentTypeHelper;
 import com.angkorteam.fintech.pages.OrganizationDashboardPage;
 import com.angkorteam.fintech.widget.TextFeedbackPanel;
+import com.angkorteam.fintech.widget.WebMarkupBlock;
+import com.angkorteam.fintech.widget.WebMarkupBlock.Size;
 import com.angkorteam.framework.models.PageBreadcrumb;
 import com.angkorteam.framework.wicket.markup.html.form.Button;
 import com.angkorteam.framework.wicket.markup.html.form.Form;
@@ -29,29 +31,37 @@ import com.mashape.unirest.http.exceptions.UnirestException;
  * Created by socheatkhauv on 6/26/17.
  */
 @AuthorizeInstantiation(Function.ALL_FUNCTION)
-public class PaymentTypeCreatePage extends DeprecatedPage {
+public class PaymentTypeCreatePage extends Page {
 
-    private String descriptionValue;
-    private TextField<String> descriptionField;
-    private TextFeedbackPanel descriptionFeedback;
+    protected WebMarkupBlock descriptionBlock;
+    protected WebMarkupContainer descriptionIContainer;
+    protected String descriptionValue;
+    protected TextField<String> descriptionField;
+    protected TextFeedbackPanel descriptionFeedback;
 
-    private String nameValue;
-    private TextField<String> nameField;
-    private TextFeedbackPanel nameFeedback;
+    protected WebMarkupBlock nameBlock;
+    protected WebMarkupContainer nameIContainer;
+    protected String nameValue;
+    protected TextField<String> nameField;
+    protected TextFeedbackPanel nameFeedback;
 
-    private Integer positionValue;
-    private TextField<Integer> positionField;
-    private TextFeedbackPanel positionFeedback;
+    protected WebMarkupBlock positionBlock;
+    protected WebMarkupContainer positionIContainer;
+    protected Integer positionValue;
+    protected TextField<Integer> positionField;
+    protected TextFeedbackPanel positionFeedback;
 
-    private Boolean cashValue;
-    private CheckBox cashField;
-    private TextFeedbackPanel cashFeedback;
+    protected WebMarkupBlock cashBlock;
+    protected WebMarkupContainer cashIContainer;
+    protected Boolean cashValue;
+    protected CheckBox cashField;
+    protected TextFeedbackPanel cashFeedback;
 
-    private Form<Void> form;
-    private Button saveButton;
-    private BookmarkablePageLink<Void> closeLink;
+    protected Form<Void> form;
+    protected Button saveButton;
+    protected BookmarkablePageLink<Void> closeLink;
 
-    private static final List<PageBreadcrumb> BREADCRUMB;
+    protected static final List<PageBreadcrumb> BREADCRUMB;
 
     @Override
     public IModel<List<PageBreadcrumb>> buildPageBreadcrumb() {
@@ -85,9 +95,11 @@ public class PaymentTypeCreatePage extends DeprecatedPage {
     }
 
     @Override
-    protected void onInitialize() {
-        super.onInitialize();
+    protected void initData() {
+    }
 
+    @Override
+    protected void initComponent() {
         this.form = new Form<>("form");
         add(this.form);
 
@@ -98,32 +110,72 @@ public class PaymentTypeCreatePage extends DeprecatedPage {
         this.closeLink = new BookmarkablePageLink<>("closeLink", PaymentTypeBrowsePage.class);
         this.form.add(this.closeLink);
 
-        this.descriptionField = new TextField<>("descriptionField", new PropertyModel<>(this, "descriptionValue"));
-        this.descriptionField.setRequired(true);
-        this.form.add(this.descriptionField);
-        this.descriptionFeedback = new TextFeedbackPanel("descriptionFeedback", this.descriptionField);
-        this.form.add(this.descriptionFeedback);
+        initDescriptionBlock();
 
-        this.nameField = new TextField<>("nameField", new PropertyModel<>(this, "nameValue"));
-        this.nameField.setRequired(true);
-        this.form.add(this.nameField);
-        this.nameFeedback = new TextFeedbackPanel("nameFeedback", this.nameField);
-        this.form.add(this.nameFeedback);
+        initNameBlock();
 
-        this.cashField = new CheckBox("cashField", new PropertyModel<>(this, "cashValue"));
-        this.cashField.setRequired(true);
-        this.form.add(this.cashField);
-        this.cashFeedback = new TextFeedbackPanel("cashFeedback", this.cashField);
-        this.form.add(this.cashFeedback);
+        initCashBlock();
 
-        this.positionField = new TextField<>("positionField", new PropertyModel<>(this, "positionValue"));
-        this.positionField.setRequired(true);
-        this.form.add(this.positionField);
-        this.positionFeedback = new TextFeedbackPanel("positionFeedback", this.positionField);
-        this.form.add(this.positionFeedback);
+        initPositionBlock();
     }
 
-    private void saveButtonSubmit(Button button) {
+    @Override
+    protected void configureRequiredValidation() {
+    }
+
+    @Override
+    protected void configureMetaData() {
+    }
+
+    protected void initPositionBlock() {
+        this.positionBlock = new WebMarkupBlock("positionBlock", Size.Twelve_12);
+        this.form.add(this.positionBlock);
+        this.positionIContainer = new WebMarkupContainer("positionIContainer");
+        this.positionBlock.add(this.positionIContainer);
+        this.positionField = new TextField<>("positionField", new PropertyModel<>(this, "positionValue"));
+        this.positionField.setRequired(true);
+        this.positionIContainer.add(this.positionField);
+        this.positionFeedback = new TextFeedbackPanel("positionFeedback", this.positionField);
+        this.positionIContainer.add(this.positionFeedback);
+    }
+
+    protected void initCashBlock() {
+        this.cashBlock = new WebMarkupBlock("cashBlock", Size.Twelve_12);
+        this.form.add(this.cashBlock);
+        this.cashIContainer = new WebMarkupContainer("cashIContainer");
+        this.cashBlock.add(this.cashIContainer);
+        this.cashField = new CheckBox("cashField", new PropertyModel<>(this, "cashValue"));
+        this.cashField.setRequired(true);
+        this.cashIContainer.add(this.cashField);
+        this.cashFeedback = new TextFeedbackPanel("cashFeedback", this.cashField);
+        this.cashIContainer.add(this.cashFeedback);
+    }
+
+    protected void initNameBlock() {
+        this.nameBlock = new WebMarkupBlock("nameBlock", Size.Twelve_12);
+        this.form.add(this.nameBlock);
+        this.nameIContainer = new WebMarkupContainer("nameIContainer");
+        this.nameBlock.add(this.nameIContainer);
+        this.nameField = new TextField<>("nameField", new PropertyModel<>(this, "nameValue"));
+        this.nameField.setRequired(true);
+        this.nameIContainer.add(this.nameField);
+        this.nameFeedback = new TextFeedbackPanel("nameFeedback", this.nameField);
+        this.nameIContainer.add(this.nameFeedback);
+    }
+
+    protected void initDescriptionBlock() {
+        this.descriptionBlock = new WebMarkupBlock("descriptionBlock", Size.Twelve_12);
+        this.form.add(this.descriptionBlock);
+        this.descriptionIContainer = new WebMarkupContainer("descriptionIContainer");
+        this.descriptionBlock.add(this.descriptionIContainer);
+        this.descriptionField = new TextField<>("descriptionField", new PropertyModel<>(this, "descriptionValue"));
+        this.descriptionField.setRequired(true);
+        this.descriptionIContainer.add(this.descriptionField);
+        this.descriptionFeedback = new TextFeedbackPanel("descriptionFeedback", this.descriptionField);
+        this.descriptionIContainer.add(this.descriptionFeedback);
+    }
+
+    protected void saveButtonSubmit(Button button) {
         PaymentTypeBuilder builder = new PaymentTypeBuilder();
         builder.withName(this.nameValue);
         builder.withPosition(this.positionValue);
