9c8d6799756ed78016dda4e19735edd3115fb9b5
Socheat KHAUV <pkayjava@gmail.com>
2017-11-11 16:44:40 +0800

    ClientDefaultSavingAccountPage

--- src/main/java/com/angkorteam/fintech/pages/client/client/ClientDefaultSavingAccountPage.java
+++ src/main/java/com/angkorteam/fintech/pages/client/client/ClientDefaultSavingAccountPage.java
@@ -9,14 +9,15 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-import com.angkorteam.fintech.DeprecatedPage;
-import com.angkorteam.fintech.DeprecatedPage;
+import com.angkorteam.fintech.Page;
 import com.angkorteam.fintech.Session;
 import com.angkorteam.fintech.dto.Function;
 import com.angkorteam.fintech.dto.builder.client.client.ClientDefaulSavingAccountBuilder;
 import com.angkorteam.fintech.helper.ClientHelper;
 import com.angkorteam.fintech.provider.SingleChoiceProvider;
 import com.angkorteam.fintech.widget.TextFeedbackPanel;
+import com.angkorteam.fintech.widget.WebMarkupBlock;
+import com.angkorteam.fintech.widget.WebMarkupBlock.Size;
 import com.angkorteam.framework.SpringBean;
 import com.angkorteam.framework.spring.JdbcTemplate;
 import com.angkorteam.framework.wicket.markup.html.form.Button;
@@ -27,7 +28,7 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 @AuthorizeInstantiation(Function.ALL_FUNCTION)
-public class ClientDefaultSavingAccountPage extends DeprecatedPage {
+public class ClientDefaultSavingAccountPage extends Page {
 
     protected String clientId;
 
@@ -35,19 +36,15 @@ public class ClientDefaultSavingAccountPage extends DeprecatedPage {
     protected Button saveButton;
     protected BookmarkablePageLink<Void> closeLink;
 
-    protected WebMarkupContainer savingBlock;
-    protected WebMarkupContainer savingContainer;
+    protected WebMarkupBlock savingBlock;
+    protected WebMarkupContainer savingIContainer;
     protected SingleChoiceProvider savingProvider;
     protected Option savingValue;
     protected Select2SingleChoice<Option> savingField;
     protected TextFeedbackPanel savingFeedback;
 
     @Override
-    protected void onInitialize() {
-        super.onInitialize();
-
-        initData();
-
+    protected void initComponent() {
         PageParameters parameters = new PageParameters();
         parameters.add("clientId", this.clientId);
 
@@ -61,10 +58,24 @@ public class ClientDefaultSavingAccountPage extends DeprecatedPage {
         this.closeLink = new BookmarkablePageLink<>("closeLink", ClientPreviewPage.class, parameters);
         this.form.add(this.closeLink);
 
-        this.savingBlock = new WebMarkupContainer("savingBlock");
+        initSavingBlock();
+    }
+
+    @Override
+    protected void configureRequiredValidation() {
+
+    }
+
+    @Override
+    protected void configureMetaData() {
+
+    }
+
+    protected void initSavingBlock() {
+        this.savingBlock = new WebMarkupBlock("savingBlock", Size.Six_6);
         this.form.add(this.savingBlock);
-        this.savingContainer = new WebMarkupContainer("savingContainer");
-        this.savingBlock.add(this.savingContainer);
+        this.savingIContainer = new WebMarkupContainer("savingIContainer");
+        this.savingBlock.add(this.savingIContainer);
         this.savingProvider = new SingleChoiceProvider("m_savings_account", "m_savings_account.id", "m_savings_account.account_no", "concat(m_savings_account.account_no, ' => ', m_savings_product.name)");
         this.savingProvider.addJoin("INNER JOIN m_savings_product ON m_savings_account.product_id = m_savings_product.id");
         this.savingProvider.applyWhere("status_enum", "m_savings_account.status_enum = 300");
@@ -72,11 +83,12 @@ public class ClientDefaultSavingAccountPage extends DeprecatedPage {
         this.savingField = new Select2SingleChoice<>("savingField", new PropertyModel<>(this, "savingValue"), this.savingProvider);
         this.savingField.setRequired(true);
         this.savingField.setLabel(Model.of("Saving Account"));
-        this.savingContainer.add(this.savingField);
+        this.savingIContainer.add(this.savingField);
         this.savingFeedback = new TextFeedbackPanel("savingFeedback", this.savingField);
-        this.savingContainer.add(this.savingFeedback);
+        this.savingIContainer.add(this.savingFeedback);
     }
 
+    @Override
     protected void initData() {
         JdbcTemplate jdbcTemplate = SpringBean.getBean(JdbcTemplate.class);
         this.clientId = getPageParameters().get("clientId").toString();
