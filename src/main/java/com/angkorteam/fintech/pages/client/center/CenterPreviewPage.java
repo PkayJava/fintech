66c2e2019783c013f01d4f79d1a4617b91607605
Socheat KHAUV <pkayjava@gmail.com>
2017-11-11 13:46:19 +0800

    CenterPreviewPage

--- src/main/java/com/angkorteam/fintech/pages/client/center/CenterPreviewPage.java
+++ src/main/java/com/angkorteam/fintech/pages/client/center/CenterPreviewPage.java
@@ -6,31 +6,39 @@ import org.apache.wicket.authroles.authorization.strategies.role.annotations.Aut
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-import com.angkorteam.fintech.DeprecatedPage;
-import com.angkorteam.fintech.DeprecatedPage;
+import com.angkorteam.fintech.Page;
 import com.angkorteam.fintech.dto.Function;
 import com.angkorteam.fintech.widget.center.CenterPreviewGeneral;
 import com.angkorteam.fintech.widget.center.CenterPreviewNote;
 import com.angkorteam.framework.wicket.extensions.markup.html.tabs.AjaxTabbedPanel;
 
 @AuthorizeInstantiation(Function.ALL_FUNCTION)
-public class CenterPreviewPage extends DeprecatedPage {
+public class CenterPreviewPage extends Page {
 
     private AjaxTabbedPanel<ITab> tab;
 
     protected String centerId;
 
     @Override
-    protected void onInitialize() {
-        super.onInitialize();
-
+    protected void initData() {
         PageParameters parameters = getPageParameters();
-
         this.centerId = parameters.get("centerId").toString();
+    }
 
+    @Override
+    protected void initComponent() {
         this.tab = new AjaxTabbedPanel<>("tab", Arrays.asList(new CenterPreviewGeneral(this), new CenterPreviewNote(this)));
-
         add(this.tab);
+    }
+
+    @Override
+    protected void configureRequiredValidation() {
+
+    }
+
+    @Override
+    protected void configureMetaData() {
+
     }
 
 }
