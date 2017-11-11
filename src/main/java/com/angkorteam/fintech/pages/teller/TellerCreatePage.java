8041356759f8a7744fda0b24061c89753ad886fe
Socheat KHAUV <pkayjava@gmail.com>
2017-11-11 18:41:13 +0800

    teller

--- src/main/java/com/angkorteam/fintech/pages/teller/TellerCreatePage.java
+++ src/main/java/com/angkorteam/fintech/pages/teller/TellerCreatePage.java
@@ -12,7 +12,7 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 
-import com.angkorteam.fintech.DeprecatedPage;
+import com.angkorteam.fintech.Page;
 import com.angkorteam.fintech.Session;
 import com.angkorteam.fintech.dto.Function;
 import com.angkorteam.fintech.dto.builder.TellerBuilder;
@@ -38,7 +38,7 @@ import com.mashape.unirest.http.exceptions.UnirestException;
  * Created by socheatkhauv on 7/13/17.
  */
 @AuthorizeInstantiation(Function.ALL_FUNCTION)
-public class TellerCreatePage extends DeprecatedPage {
+public class TellerCreatePage extends Page {
 
     protected Form<Void> form;
     protected Button saveButton;
--- src/main/java/com/angkorteam/fintech/pages/teller/TellerModifyPage.java
+++ src/main/java/com/angkorteam/fintech/pages/teller/TellerModifyPage.java
@@ -14,7 +14,7 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-import com.angkorteam.fintech.DeprecatedPage;
+import com.angkorteam.fintech.Page;
 import com.angkorteam.fintech.Session;
 import com.angkorteam.fintech.dto.Function;
 import com.angkorteam.fintech.dto.builder.TellerBuilder;
@@ -42,7 +42,7 @@ import com.mashape.unirest.http.exceptions.UnirestException;
  * Created by socheatkhauv on 7/13/17.
  */
 @AuthorizeInstantiation(Function.ALL_FUNCTION)
-public class TellerModifyPage extends DeprecatedPage {
+public class TellerModifyPage extends Page {
 
     protected String tellerId;
 
