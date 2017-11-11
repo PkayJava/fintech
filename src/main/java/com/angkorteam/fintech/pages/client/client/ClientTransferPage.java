06542b7380fde792dbb2c92566ab22db23d32b5e
Socheat KHAUV <pkayjava@gmail.com>
2017-11-11 17:06:08 +0800

    ClientTransferPage

--- src/main/java/com/angkorteam/fintech/pages/client/client/ClientTransferPage.java
+++ src/main/java/com/angkorteam/fintech/pages/client/client/ClientTransferPage.java
@@ -10,14 +10,15 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-import com.angkorteam.fintech.DeprecatedPage;
-import com.angkorteam.fintech.DeprecatedPage;
+import com.angkorteam.fintech.Page;
 import com.angkorteam.fintech.Session;
 import com.angkorteam.fintech.dto.Function;
 import com.angkorteam.fintech.dto.builder.client.client.ClientTransferBuilder;
 import com.angkorteam.fintech.helper.ClientHelper;
 import com.angkorteam.fintech.provider.SingleChoiceProvider;
 import com.angkorteam.fintech.widget.TextFeedbackPanel;
+import com.angkorteam.fintech.widget.WebMarkupBlock;
+import com.angkorteam.fintech.widget.WebMarkupBlock.Size;
 import com.angkorteam.framework.SpringBean;
 import com.angkorteam.framework.spring.JdbcTemplate;
 import com.angkorteam.framework.wicket.markup.html.form.Button;
@@ -28,7 +29,7 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 @AuthorizeInstantiation(Function.ALL_FUNCTION)
-public class ClientTransferPage extends DeprecatedPage {
+public class ClientTransferPage extends Page {
 
     protected String clientId;
 
@@ -36,25 +37,21 @@ public class ClientTransferPage extends DeprecatedPage {
     protected Button saveButton;
     protected BookmarkablePageLink<Void> closeLink;
 
-    protected WebMarkupContainer officeBlock;
-    protected WebMarkupContainer officeContainer;
+    protected WebMarkupBlock officeBlock;
+    protected WebMarkupContainer officeIContainer;
     protected SingleChoiceProvider officeProvider;
     protected Option officeValue;
     protected Select2SingleChoice<Option> officeField;
     protected TextFeedbackPanel officeFeedback;
 
-    protected WebMarkupContainer noteBlock;
-    protected WebMarkupContainer noteContainer;
+    protected WebMarkupBlock noteBlock;
+    protected WebMarkupContainer noteIContainer;
     protected String noteValue;
     protected TextArea<String> noteField;
     protected TextFeedbackPanel noteFeedback;
 
     @Override
-    protected void onInitialize() {
-        super.onInitialize();
-
-        initData();
-
+    protected void initComponent() {
         PageParameters parameters = new PageParameters();
         parameters.add("clientId", this.clientId);
 
@@ -68,29 +65,46 @@ public class ClientTransferPage extends DeprecatedPage {
         this.closeLink = new BookmarkablePageLink<>("closeLink", ClientPreviewPage.class, parameters);
         this.form.add(this.closeLink);
 
-        this.officeBlock = new WebMarkupContainer("officeBlock");
+        initOfficeBlock();
+
+        initNoteBlock();
+    }
+
+    @Override
+    protected void configureRequiredValidation() {
+    }
+
+    @Override
+    protected void configureMetaData() {
+    }
+
+    protected void initNoteBlock() {
+        this.noteBlock = new WebMarkupBlock("noteBlock", Size.Six_6);
+        this.noteBlock.setOutputMarkupId(true);
+        this.form.add(this.noteBlock);
+        this.noteIContainer = new WebMarkupContainer("noteIContainer");
+        this.noteBlock.add(this.noteIContainer);
+        this.noteField = new TextArea<>("noteField", new PropertyModel<>(this, "noteValue"));
+        this.noteField.setLabel(Model.of("Note"));
+        this.noteIContainer.add(this.noteField);
+        this.noteFeedback = new TextFeedbackPanel("noteFeedback", this.noteField);
+        this.noteIContainer.add(this.noteFeedback);
+    }
+
+    protected void initOfficeBlock() {
+        this.officeBlock = new WebMarkupBlock("officeBlock", Size.Six_6);
         this.form.add(this.officeBlock);
-        this.officeContainer = new WebMarkupContainer("officeContainer");
-        this.officeBlock.add(this.officeContainer);
+        this.officeIContainer = new WebMarkupContainer("officeIContainer");
+        this.officeBlock.add(this.officeIContainer);
         this.officeProvider = new SingleChoiceProvider("m_office", "id", "name");
         this.officeField = new Select2SingleChoice<>("officeField", new PropertyModel<>(this, "officeValue"), this.officeProvider);
         this.officeField.setRequired(true);
-        this.officeContainer.add(this.officeField);
+        this.officeIContainer.add(this.officeField);
         this.officeFeedback = new TextFeedbackPanel("officeFeedback", this.officeField);
-        this.officeContainer.add(this.officeFeedback);
-
-        this.noteBlock = new WebMarkupContainer("noteBlock");
-        this.noteBlock.setOutputMarkupId(true);
-        this.form.add(this.noteBlock);
-        this.noteContainer = new WebMarkupContainer("noteContainer");
-        this.noteBlock.add(this.noteContainer);
-        this.noteField = new TextArea<>("noteField", new PropertyModel<>(this, "noteValue"));
-        this.noteField.setLabel(Model.of("Note"));
-        this.noteContainer.add(this.noteField);
-        this.noteFeedback = new TextFeedbackPanel("noteFeedback", this.noteField);
-        this.noteContainer.add(this.noteFeedback);
+        this.officeIContainer.add(this.officeFeedback);
     }
 
+    @Override
     protected void initData() {
         JdbcTemplate jdbcTemplate = SpringBean.getBean(JdbcTemplate.class);
         this.clientId = getPageParameters().get("clientId").toString();
