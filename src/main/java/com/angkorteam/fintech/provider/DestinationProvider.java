849c3a4181a205d570ab7832925fd4fa926ddd6e
Socheat KHAUV <pkayjava@gmail.com>
2017-12-22 17:21:10 +0800

    standing instruction

---
+++ src/main/java/com/angkorteam/fintech/provider/DestinationProvider.java
@@ -0,0 +1,62 @@
+package com.angkorteam.fintech.provider;
+
+import java.util.List;
+
+import org.apache.wicket.model.IModel;
+
+import com.angkorteam.fintech.dto.enums.Destination;
+import com.angkorteam.fintech.dto.enums.Status;
+import com.angkorteam.framework.wicket.markup.html.form.select2.Option;
+import com.angkorteam.framework.wicket.markup.html.form.select2.SingleChoiceProvider;
+import com.google.common.collect.Lists;
+
+/**
+ * Created by socheatkhauv on 7/1/17.
+ */
+public class DestinationProvider extends SingleChoiceProvider<Option> {
+
+    @Override
+    public Option toChoice(String id) {
+        List<Option> options = query("", 0);
+        for (Option option : options) {
+            if (option.getId().equals(id)) {
+                return option;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Option> query(String term, int page) {
+        List<Option> options = Lists.newArrayList();
+        for (Destination value : Destination.values()) {
+            options.add(value.toOption());
+        }
+        return options;
+    }
+
+    @Override
+    public boolean hasMore(String term, int page) {
+        return false;
+    }
+
+    @Override
+    public Object getDisplayValue(Option object) {
+        return object.getText();
+    }
+
+    @Override
+    public String getIdValue(Option object, int index) {
+        return object.getId();
+    }
+
+    @Override
+    public Option getObject(String id, IModel<? extends List<? extends Option>> choices) {
+        for (Option option : choices.getObject()) {
+            if (option.getId().equals(id)) {
+                return option;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/angkorteam/fintech/provider/OfficeProvider.java
@@ -0,0 +1,9 @@
+package com.angkorteam.fintech.provider;
+
+public class OfficeProvider extends SingleChoiceProvider {
+
+    public OfficeProvider() {
+        super("m_office", "id", "name");
+    }
+
+}
