8237935a1f50a09e66328a75b901d74626916dce
Socheat KHAUV <pkayjava@gmail.com>
2018-02-25 21:25:34 +0700

    pages teller

--- src/main/java/com/angkorteam/fintech/ddl/MTellers.java
+++ src/main/java/com/angkorteam/fintech/ddl/MTellers.java
@@ -2,19 +2,27 @@ package com.angkorteam.fintech.ddl;
 
 public interface MTellers {
 
-    public static final String NAME = "m_template";
+    public static final String NAME = "m_tellers";
 
     public interface Field {
 
         public static final String ID = "id";
 
+        public static final String OFFICE_ID = "office_id";
+
+        public static final String DEBIT_ACCOUNT_ID = "debit_account_id";
+
+        public static final String CREDIT_ACCOUNT_ID = "credit_account_id";
+
         public static final String NAME = "name";
 
-        public static final String TEXT = "text";
+        public static final String DESCRIPTION = "description";
 
-        public static final String ENTITY = "entity";
+        public static final String VALID_FROM = "valid_from";
 
-        public static final String TYPE = "type";
+        public static final String VALID_TO = "valid_to";
+
+        public static final String STATE = "state";
 
     }
 
